49080d36dbbb3f14b59cf0b8cf899b2d71284ace
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-10-30 12:36:08 -0500

    config: agregando liquibase al proyecto

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("io.jsonwebtoken:jjwt-api:0.13.0")
-	implementation("org.flywaydb:flyway-core")
+	implementation("org.liquibase:liquibase-core:4.33.0")
+	implementation("org.projectlombok:lombok")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.13.0")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.13.0") // or 'io.jsonwebtoken:jjwt-gson:0.13.0' for gson
 	runtimeOnly("org.postgresql:postgresql")
