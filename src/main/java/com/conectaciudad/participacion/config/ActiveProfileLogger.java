0f22adda89a360fd00848c312d67052b465b94bb
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-10-30 18:48:55 -0500

    fix: arreglando logger para el active profile

--- src/main/java/com/conectaciudad/participacion/config/ActiveProfileLogger.java
+++ src/main/java/com/conectaciudad/participacion/config/ActiveProfileLogger.java
@@ -1,11 +1,11 @@
 package com.conectaciudad.participacion.config;
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-@Slf4j
+@Log4j2
 @Component
 public class ActiveProfileLogger implements CommandLineRunner {
 
@@ -14,6 +14,6 @@ public class ActiveProfileLogger implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        log.info("Spring active profile: {}", activeProfile);
+        log.info("Spring active profile: "+ activeProfile);
     }
 }
