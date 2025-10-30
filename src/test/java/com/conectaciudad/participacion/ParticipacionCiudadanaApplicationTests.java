54983dd997c23a53caa3f3b5394f8baf8b3a2d78
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-10-30 00:49:32 -0500

    config: creando la estructura inicial del proyecto

--- src/test/java/com/conectaciudad/participacion/ParticipacionCiudadanaApplicationTests.java
+++ src/test/java/com/conectaciudad/participacion/ParticipacionCiudadanaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.conectaciudad.participacion_ciudadana;
+package com.conectaciudad.participacion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
