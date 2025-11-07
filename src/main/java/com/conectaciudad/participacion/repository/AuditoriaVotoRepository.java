19cdc2b75d9081e606b929a1f59c6b7b3a2b0054
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-11-06 22:02:35 -0500

    fix: renombrano de AuditoriaRepository.java a AuditoriaVotoRepository

--- src/main/java/com/conectaciudad/participacion/repository/AuditoriaVotoRepository.java
+++ src/main/java/com/conectaciudad/participacion/repository/AuditoriaVotoRepository.java
@@ -1,13 +1,9 @@
 package com.conectaciudad.participacion.repository;
 
-import com.conectaciudad.participacion.model.AlertaAuditoria;
 import com.conectaciudad.participacion.model.AuditoriaVoto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @Repository
-public interface AuditoriaRepository extends JpaRepository<AlertaAuditoria, Long> {
+public interface AuditoriaVotoRepository extends JpaRepository<AuditoriaVoto, Long> {
 }
