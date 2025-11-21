3a14890930315f22d78aaba831baf13e32ebb4f2
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-11-21 13:38:19 -0500

    fix: aceptando autenticacion proveniente del controller para procesar y sacar el id del ciudadano

--- src/main/java/com/conectaciudad/participacion/service/VotacionService.java
+++ src/main/java/com/conectaciudad/participacion/service/VotacionService.java
@@ -3,11 +3,12 @@ package com.conectaciudad.participacion.service;
 import com.conectaciudad.participacion.dto.RespuestaVotoDTO;
 import com.conectaciudad.participacion.dto.ResultadoVotacionDTO;
 import com.conectaciudad.participacion.dto.VotoDetailDTO;
+import org.springframework.security.core.Authentication;
 
 public interface VotacionService {
     VotoDetailDTO obtenerVoto(Long votacionId);
-    RespuestaVotoDTO registrarVoto(Long idProyecto, boolean decision, Long ciudadanoId);
+    RespuestaVotoDTO registrarVoto(Long idProyecto, boolean decision, Authentication authentication);
     ResultadoVotacionDTO obtenerResultadosPorProyecto(Long idProyecto);
 
-    VotoDetailDTO obtenerVotoPorCiudadanoYProyecto(Long ciudadanoId, Long idProyecto);
+    VotoDetailDTO obtenerVotoPorCiudadanoYProyecto(Authentication authentication, Long ciudadanoId);
 }
