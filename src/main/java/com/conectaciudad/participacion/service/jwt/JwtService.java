c5998709e98fbf5a47aa0ab56861f48b1a75a420
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-11-21 00:14:02 -0500

    fix: decodificando secret raw en lugar de base 64

--- src/main/java/com/conectaciudad/participacion/service/jwt/JwtService.java
+++ src/main/java/com/conectaciudad/participacion/service/jwt/JwtService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.List;
 import java.util.function.Function;
@@ -25,8 +26,9 @@ public class JwtService {
     private String issuer;
 
     private SecretKey getSigningKey() {
-        byte[] passwordDecoded = Decoders.BASE64.decode(secretKey);
-        return Keys.hmacShaKeyFor(passwordDecoded);
+        //byte[] passwordDecoded = Decoders.BASE64.decode(secretKey);
+        //return Keys.hmacShaKeyFor(passwordDecoded);
+        return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
     }
 
     public Claims extractAllClaims(String token) {
